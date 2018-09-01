5f580245d8ff339c3bb9d7f119c7ff1a8afcd42f
Video Communication <liskovyakov@gmail.com>
2018-09-01 17:06:29 +0300

    add interface

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 'use strict';
 
 module.exports = require('./lib/dafsm');
+module.exports = require('./lib/fdafsm');
+module.exports.fsmChart = require('./cli/primitives/fsm');
+module.exports.jsPatern = require('./cli/paterns/javascript');
