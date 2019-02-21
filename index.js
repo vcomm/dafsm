a702f0d0816bf6c61aceeb088011fc0eae20128b
vcomm <40527636+vcomm@users.noreply.github.com>
2019-02-21 10:17:11 +0200

    comment component for code generation

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ module.exports = require('./lib/dafsm');
 module.exports = require('./lib/fdafsm');
 module.exports = require('./lib/cdafsm');
 module.exports = require('./lib/pdafsm');
+/*
 module.exports.fsmChart = require('./cli/primitives/fsm');
 module.exports.jsPatern = require('./cli/paterns/javascript');
 module.exports.es6Patern = require('./cli/paterns/es6');
 module.exports.swiftPatern = require('./cli/paterns/swift');
 module.exports.javaPatern = require('./cli/paterns/java');
+*/
